ee613c59b18afb9a37b7ac851d1fad86bfee5179
AndrewEl131 <andyelizbarashvilii@gmail.com>
2026-02-06 19:48:33 +0400

    Added jounal page

---
+++ app/journal/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main className='w-full pt-15'>
+        <div className='w-full h-120 journal-ticket'></div>
+        <div>
+            
+        </div>
+    </main>
+  )
+}
